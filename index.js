4d38830537d93c01ac25f3de7201bd790dc84ead
Apple <apple@Apples-MacBook-Pro-2.local>
2019-06-14 17:44:26 -0500

    linking/pictures

---
+++ index.js
@@ -0,0 +1,16 @@
+var Name = $('.card-container');
+var Favorite-chocolate = $('.card-container');
+var Tempature = $('.card-container');
+var submit = $('.submit-btn');
+
+
+submit.on("click");
+
+function chocolate() {
+  var name = $('.name').val();
+  var Favorite-chocolate = $('.Favorite-chocolate').val();
+    var Tempature = $('.Tempature').val();
+
+  $('.name').val("");
+
+}
